12f2bd17e785b867ddb880777c605597cedafdf9
hyperandroid <hyperandroid@gmail.com>
2015-05-11 14:37:41 +0200

    Added some calss objects documentation.

--- app/src/main/jni/JavaDispatcher.h
+++ app/src/main/jni/JavaDispatcher.h
@@ -9,6 +9,44 @@
 
 namespace sdkbox {
 
+    /**
+     * The JavaDispatcher is a Booch utility object which knows how to communicate with the Java
+     * side JavaDispatcher object.
+     * Its main responsibility is to marshall calls from Native to Java and handle results. To do so,
+     * the developer has not to deal with Java's native method signatures. These will be derived in
+     * Java and upon method call, the resulting object of the Java method invocation will be given
+     * as return value.
+     *
+     * There are 3 methods to communicate with Java side. In all of them, the Java-side method
+     * arguments are supplied as a <code>jobjectArray</code>. See <code>JNIUtils</code> for a
+     * convenient way of dealing with these objects.
+     *
+     * <b>callInService</b>
+     * This method will invoke a method that belongs to a Java Service object. The service will be
+     * identified by its canonical Java class representation. Then, the method name and its arguments
+     * must be supplied to the method call.
+     * The java method invoked will be inferred from the arguments' signature (class type).
+     * Primitive types will, before method inference, be unboxed.
+     * If no suitable method exists for the service, or there's an error, or the invoked method
+     * is <code>void</code>, null will be returned.
+     *
+     * <b>callStatic</b>
+     * This method will invoke an arbitrary static method in any Class object. This method needs
+     * the canonical name of a class, as well as the method name and its invocation arguments.
+     * Same restrictions that in <code>callInService</code> apply to this method.
+     *
+     * <b>callInInstance</b>
+     * This method will invoke ant method in a given object instance. The object instance can be
+     * obtained using regular JNI calls, passing it from Java to Native, or by using a
+     * <code>Proxy</code> object.
+     * In this case, the method needs a <code>jobject</code> Java reference, as well as the method
+     * name and its arguments.
+     * Same restrictions apply to the arguments than in previous methods.
+     *
+     * Most of the DynamicJNI functionalities will rely on these methods to perform their work.
+     *
+     * Warning: this class is useless without its Java JavaDispatcher.java object counterpart.
+     */
     class JavaDispatcher {
 
     private:
--- app/src/main/jni/Proxy.h
+++ app/src/main/jni/Proxy.h
@@ -12,6 +12,24 @@ class Proxy;
 typedef shared_ptr<Proxy>       SPProxy;
 typedef shared_ptr<const Proxy> CSPProxy;
 
+/**
+ * A Proxy is a native object that holds a Java object reference of a given type.
+ * The Proxy is built by supplying a canonical class name, and a set of parameters that will be
+ * passed to the suitable Java Class constructor.
+ * The constructor is inferred based on the parameter types.
+ * The result of this constructor invocation will be kept as the Java reference.
+ * Successive calls to invoke will invoke a the associated Java method for the reference class
+ * with the supplied parameters.
+ * The return value will be NULL if either the method returns void or an error has occurred.
+ *
+ * The way to build a Proxy object is by using its static New method like:
+ * <code>
+ *  Proxy::New( "com/cocos2dx/example/TestProxy", ... );
+ * </code>
+ *
+ * It is expected the developer will extends this Class with his own proxy types, where each method
+ * will internally call invoke to proxy the Java object's function.
+ */
 class Proxy {
 protected:
 
@@ -20,12 +38,12 @@ protected:
 
     Proxy( const char* clazz );
 
-    void init( jobjectArray constructorParams );
+    virtual void init( jobjectArray constructorParams );
 
 public:
-    ~Proxy();
+    virtual ~Proxy();
 
-    jobject invoke( const char* method, const jobjectArray args );
+    virtual jobject invoke( const char* method, const jobjectArray args );
 
     static SPProxy New( const char* clazz, jobjectArray constructorParams );
 };
